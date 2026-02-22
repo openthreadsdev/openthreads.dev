eb87a2837911f6fda5d4f73b91c80cc68c614570
kahboom <rachelyordan@gmail.com>
2026-02-22 23:22:30 +0000

    fix: add Buffer polyfill for gray-matter in browser

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
+import { Buffer } from "buffer";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+
+// Polyfill Buffer for gray-matter library // spelling:disable
+window.Buffer = Buffer;
 
 // Update document title to show development mode
 if (import.meta.env.DEV) {
